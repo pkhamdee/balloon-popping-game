80d0ca1f1bcf6abc94e5e4959bd8c9a4fe58f9a4
Pongsak Khamdee <pongsakk@pongsakkPT69N.vmware.com>
2023-07-23 11:48:57 +0700

    seeding merchant sql

--- init.sql
+++ init.sql
@@ -1,2 +1,3 @@
 CREATE TABLE "player" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "price" integer NOT NULL, CONSTRAINT "PK_65edadc946a7faf4b638d5e8885" PRIMARY KEY ("id"));
 CREATE TABLE "merchant" ("id" SERIAL NOT NULL, "product" character varying NOT NULL, "image" character varying NOT NULL, "price" integer NOT NULL, "status" boolean NOT NULL, "hint" character varying NOT NULL, CONSTRAINT "PK_9a3850e0537d869734fc9bff5d6" PRIMARY KEY ("id"));
+INSERT INTO "merchant" ("product", "image", "price", "status", "hint") VALUES ('VMware Jacket', 'https://emea.vmwaremerchandise.com/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/s/o/softshell_fr.jpg', 100, false, 'price start from 10$ to 500$');
