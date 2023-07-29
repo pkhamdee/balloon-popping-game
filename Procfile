2e86dce0adb6ddcba3ebd373a335b8881f94a144
Chris Angelo Averia <averiac@vmware.com>
2023-07-29 12:10:38 +0800

    fix CNB Procfile
    CNB Procfile edits

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./env.sh && nginx -p /workspace -c /workspace/nginx.conf -g pid /tmp/nginx.pid
+web: ./env.sh && nginx -p /workspace -c /workspace/nginx.conf -g 'pid /tmp/nginx.pid;'
--- env.sh
+++ env.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-# Recreate config file
-rm -rf ./env.js
-touch ./env.js
+ENV_JS_FILE=./build/env.js
 
-# Add assignment 
-echo "window._env_ = {" >> ./env.js
+# Recreate config file
+rm -rf $ENV_FILE && touch $ENV_JS_FILE
+
+# Add assignment
+echo "window.env = {" >> $ENV_JS_FILE
 
 # Read each line in .env file
 # Each line represents key=value pairs
 while read -r line || [ -n "$line" ];
 do
+  line_ctr=$(($line_ctr + 1))
+
+  # Add comma to each entry
+  [ $line_ctr -gt 1 ] && echo "," >> $ENV_JS_FILE
+
   # Split env variables by character `=`
   if printf '%s\n' "$line" | grep -q -e '='; then
     varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
@@ -23,9 +29,10 @@ do
 
   # Otherwise use value from .env file
   [ -z $value ] && value=${varvalue}
-  
+
   # Append configuration property to JS file
-  echo "  $varname: \"$value\"," >> ./env.js
+  echo -n "  \"$varname\": \"$value\"" >> $ENV_JS_FILE
+  
 done < .env
 
-echo "}" >> ./env.js
+echo "}" >> $ENV_JS_FILE
