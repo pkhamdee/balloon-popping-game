2cb677b40fa996b5201e71688e0bfd56848d1231
chungsu lee <lcscom@kakao.com>
2023-09-28 13:51:44 +0800

    test

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -125,7 +125,7 @@ function MainPage() {
                     </div>
                     <div class="col-8">
                         <br></br>
-                        <h1 style={textStyle}> Sanil's Price is Right </h1>
+                        <h1 style={textStyle}> Simin, Mukilan, Pranav's Price is Right </h1>
                     </div>
                 </div>
             </div>
